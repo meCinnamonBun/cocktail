b9f7e8c3144ed2671e2b1c900c51c18432b062fe
andrey <andrey.dovgopoliuk@bytepace.com>
2022-04-27 20:06:37 +0600

    Initial Commit

---
+++ Cocktails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktails
+//
+//  Created by Andrew on 27.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
