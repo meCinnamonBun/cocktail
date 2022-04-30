dd98678a61a1db1f3e97dccd94a0424aba10326e
andrey <andrey.dovgopoliuk@bytepace.com>
2022-04-30 18:22:00 +0600

    [+] CoctailsList screen now shows mocked data

--- Cocktails/CocktailsList/CocktailsListPresenter.swift
+++ Cocktails/CocktailsList/CocktailsListPresenter.swift
@@ -6,15 +6,29 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 protocol CocktailsListPresenterProtocol: AnyObject {
     var router: CocktailsListRouterProtocol { set get }
     var interactor: CocktailsListInteractorProtocol { set get }
+    
+    // Inputs
+    
+    // Outputs
+    
+    var cocktailsCategories: Driver<[CocktailCategory]> { get }
 }
 
 class CocktailsListPresenter: CocktailsListPresenterProtocol {
     var interactor: CocktailsListInteractorProtocol
     var router: CocktailsListRouterProtocol
+    
+    var cocktailsCategories: Driver<[CocktailCategory]> {
+        cocktailsCategoriesRelay.asDriver(onErrorJustReturn: [])
+    }
+    
+    private let cocktailsCategoriesRelay: BehaviorRelay<[CocktailCategory]> = .init(value: CocktailCategoryMocker.generateCategories()) // !!!
     
     init(router: CocktailsListRouterProtocol,
          interactor: CocktailsListInteractorProtocol) {
--- Cocktails/CocktailsList/CocktailsListViewController.swift
+++ Cocktails/CocktailsList/CocktailsListViewController.swift
@@ -6,17 +6,23 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 protocol CocktailsListViewProtocol: AnyObject {
     
 }
 
 class CocktailsListViewController: UIViewController, CocktailsListViewProtocol {
+    private let _view: CocktailsListDisplayingViewProtocol?
+    
     var presenter: CocktailsListPresenterProtocol
+    
+    private let disposeBag: DisposeBag = .init()
     
     init(presenter: CocktailsListPresenterProtocol) {
         self.presenter = presenter
-        
+        self._view = CocktailsListView()
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -24,7 +30,26 @@ class CocktailsListViewController: UIViewController, CocktailsListViewProtocol {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func loadView() {
+        self.view = _view
+    }
+    
     override func viewDidLoad() {
-        view.backgroundColor = .red
+        title = "Drinks"
+        
+        setupBindings()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    private func setupBindings() {
+        presenter.cocktailsCategories
+            .drive(onNext: { [weak self] categories in
+                self?._view?.updateView(with: categories)
+            })
+            .disposed(by: disposeBag)
     }
 }
