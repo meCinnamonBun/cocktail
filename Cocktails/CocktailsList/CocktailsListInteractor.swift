6acd5c1f777c332f03310abf65215bcc0830eca8
andrey <andrey.dovgopoliuk@bytepace.com>
2022-05-01 18:47:55 +0600

    [+] CocktailsListInteractor uses mockers

--- Cocktails/CocktailsList/CocktailsListInteractor.swift
+++ Cocktails/CocktailsList/CocktailsListInteractor.swift
@@ -6,11 +6,28 @@
 //
 
 import Foundation
+import RxSwift
 
 protocol CocktailsListInteractorProtocol: AnyObject {
-    
+    func loadCategories() -> Observable<[CocktailCategory]>
+    func loadCocktails(for category: CocktailCategory) -> Observable<CocktailsGroup>
 }
 
 class CocktailsListInteractor: CocktailsListInteractorProtocol {
+    func loadCategories() -> Observable<[CocktailCategory]> {
+        CocktailCategoriesMocker.loadCategoriesFromNetwork()
+            .map { stringCategories -> [CocktailCategory] in
+                stringCategories.map { stringCategory -> CocktailCategory in
+                    .init(displayingName: stringCategory, APIName: stringCategory)
+                }
+            }
+            .asObservable()
+    }
     
+    func loadCocktails(for category: CocktailCategory) -> Observable<CocktailsGroup> {
+        CocktailCategoriesMocker.loadCocktailsFromNetwork()
+            .map { stringCocktails -> CocktailsGroup in
+                CocktailsGroupMocker.generateEmptyNameCategory(for: stringCocktails, and: category.displayingName)
+            }
+    }
 }
