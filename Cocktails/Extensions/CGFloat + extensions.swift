c11651bedb816a7621f47dab28ccc1e6664b7b4f
andrey <andrey.dovgopoliuk@bytepace.com>
2022-05-03 19:11:08 +0600

    [+] Global CGFloat extension added

---
+++ Cocktails/Extensions/CGFloat + extensions.swift
@@ -0,0 +1,16 @@
+//
+//  CGFloat + extensions.swift
+//  Cocktails
+//
+//  Created by Andrew on 03.05.2022.
+//
+
+import UIKit
+
+extension CGFloat {
+    static let sizeM: CGFloat = 16
+    static let sizeS: CGFloat = 4
+    
+    static let fontSizeSmall: CGFloat = 9
+    static let fontSizeMedium: CGFloat = 17
+}
